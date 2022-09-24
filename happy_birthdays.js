670fdb7c6b8c19fe87271c61681a82443ca6cd07
CharlieCode666 <108889900+CharlieCode666@users.noreply.github.com>
2022-09-24 20:55:25 +0800

    Happy birthday 🎉

---
+++ happy_birthdays.js
@@ -0,0 +1,63 @@
+let showingAlert = false;
+
+const title = '生日快樂 🥳';
+const messages = '洪英筑'
+
+const changeTitle = setInterval(() => {
+  
+  document.title = 
+    showingAlert ? title : messages;
+    
+    showingAlert = !showingAlert;
+    
+}, 700);
+
+/* ====================================== */
+
+const body = document.body;
+const btn = document.querySelector('.btn');
+const switchBtn = document.querySelector('.switch');
+
+switchBtn.addEventListener('scroll', e => {
+    const y = e.target.scrollTop;
+    
+    if (y > 127) {
+        switchBtn.classList.add('active');
+        body.style.background = `rgb(${y}, ${y}, ${y})`;
+        btn.style.background = `rgb(${y}, ${y}, ${y})`;
+        switchBtn.style.background = `rgb(${y - 12}, ${y - 8}, ${y - 6})`;
+        
+  var imges = [
+  'harold1.png',
+  'harold2.png'
+  ];
+
+var num;
+var harold = document.getElementById('harold');
+
+if(y < 252) {
+        num = 1;
+        harold.src = imges[num];
+    }
+else {
+        num = 0;
+        harold.src = imges[num];
+  
+}
+    }
+    else {
+        switchBtn.classList.remove('active');
+        body.style.background = `rgb(${y + 30}, ${y + 34}, ${y + 38})`;
+        btn.style.background = `rgb(${y}, ${y + 8}, ${y + 16})`;
+        switchBtn.style.background = `rgb(${y + 20}, ${y + 24}, ${y + 28})`;
+    }
+});
+
+switchBtn.addEventListener('dragend', () => {
+    if (switchBtn.className.includes('active'))
+        switchBtn.scrollTop = 0;
+    else
+        switchBtn.scrollTop = 255;
+});
+
+
